6faef63170c4c0ecf8d831727ac10b78e94fef48
gauravtamboli <gauravtamboli.work@gmail.com>
2026-01-21 16:09:54 +0530

    update new url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://9c28-59-153-98-11.ngrok-free.app/api/KissanMitraYojnaRegisteration/',
+  // apiUrl: 'https://9c28-59-153-98-11.ngrok-free.app/api/KissanMitraYojnaRegisteration/',
+    apiUrl: 'https://nonaristocratically-frettiest-ben.ngrok-free.dev/api/KissanMitraYojnaRegisteration/', 
+
   
 
 };
