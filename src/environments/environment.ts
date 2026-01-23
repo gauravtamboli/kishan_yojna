39e1dfd65c40a5df0cc1bf331b6782290c06f541
gauravtamboli <gauravtamboli.work@gmail.com>
2026-01-23 15:10:05 +0530

    3:5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://9c28-59-153-98-11.ngrok-free.app/api/KissanMitraYojnaRegisteration/',
-   // apiUrl: 'https://nonaristocratically-frettiest-ben.ngrok-free.dev/api/KissanMitraYojnaRegisteration/', 
-  apiUrl: ' https://nonaristocratically-frettiest-ben.ngrok-free.dev/',
+    apiUrl: 'https://nonaristocratically-frettiest-ben.ngrok-free.dev/api/KissanMitraYojnaRegisteration/', 
+  
   
 
 };
